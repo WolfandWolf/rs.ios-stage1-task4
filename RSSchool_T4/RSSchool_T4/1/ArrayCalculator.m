0d6a6712a1dd3234db1a93cfab37c163ed2d1636
WolfandWolf <antonos374@GMAIL.COM>
2020-04-11 23:27:47 +0300

    feat: add task 1

--- RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
+++ RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
@@ -2,7 +2,45 @@
 
 @implementation ArrayCalculator
 + (NSInteger)maxProductOf:(NSInteger)numberOfItems itemsFromArray:(NSArray *)array {
-    // good luck
-    return -1;
+    NSMutableArray *clearArray = [array mutableCopy];
+    NSArray *sortedArray = [[NSArray alloc] init];
+    NSInteger result = 1;
+    for (NSObject *counter in array) {
+        if ([counter isKindOfClass:[NSString class]]) {
+            [clearArray removeObject:counter];
+        }
+    }
+    if ([clearArray count] == 0)
+        return 0;
+    else if ([clearArray count] <= numberOfItems) {
+        for (NSNumber *counter in clearArray) {
+            result *= [counter intValue];
+        }
+        return result;
+    }
+    else {
+        sortedArray = [clearArray sortedArrayUsingSelector:@selector(compare:)];
+        NSMutableArray *copySortedArray = [sortedArray mutableCopy];
+        int lenght = (int)[sortedArray count] - 1;
+        int counter = (int)[sortedArray count] - 1;
+        int amountOfNegative = 0;
+        for (NSNumber *counter in sortedArray) {
+            if ([counter intValue] < 0)
+                amountOfNegative++;
+        }
+        for (counter = lenght; counter >= lenght / 2; counter-=2) {
+            if ([sortedArray[lenght - counter] intValue] * -1 * [sortedArray[lenght - counter + 1] intValue] * -1 > [sortedArray[counter] intValue] * [sortedArray[counter - 1] intValue] && amountOfNegative > 1) {
+                
+                copySortedArray[lenght - counter] = [NSNumber numberWithInt:[copySortedArray[lenght - counter] intValue] * -1];
+                copySortedArray[lenght - counter + 1] = [NSNumber numberWithInt:[copySortedArray[lenght - counter + 1] intValue] * -1];
+                amountOfNegative -= 2;
+            }
+        }
+        sortedArray = [copySortedArray sortedArrayUsingSelector:@selector(compare:)];
+        for (counter = lenght; counter > lenght - numberOfItems; counter--) {
+            result *= [sortedArray[counter] intValue];
+        }
+        return result;
+    }
 }
 @end
